d2c6cc2b64ae7922572db3a5cef786862aabc277
Enrico <59199308+EnrSanti@users.noreply.github.com>
2023-12-23 15:20:07 +0100

    Delete Generative AI/Plot directory

--- Generative AI/Plot/BratGPT_files/variables.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  const variables = {"REACT_APP_GTM_ENABLED":"true"};
-
-  window.env = {};
-
-  Object.keys(variables).forEach(function(key){
-    window.env[key] = variables[key];
-  });
-}())
